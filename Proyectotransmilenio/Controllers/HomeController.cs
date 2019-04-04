be68fbc45921fe3ea9da3a03c847b0f3e34bd90e
juancarlosvalderramacamargo <juancarlosvalderramacamargo@gmail.com>
2019-04-04 12:54:24 -0500

    personalización basica

--- Proyectotransmilenio/Controllers/HomeController.cs
+++ Proyectotransmilenio/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Proyectotransmilenio.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "TRANSMILENIO";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Desarrollado por";
 
             return View();
         }
