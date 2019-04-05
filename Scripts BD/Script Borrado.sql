2b0543273480dec103ae5fefac23b6fcbd5625db
MarilynChaparro <mari.lyn1@hotmail.com>
2019-04-05 14:03:06 -0500

    Se agregan datos para poblar BD

---
+++ Scripts BD/Script Borrado.sql
@@ -0,0 +1,8 @@
+--Script Borrado de datos de la BD INFRAESTRUCTURA_TRANSMILENIO
+--Integrantes:
+--				- Marilyn Astrid Chaparro Hurtado
+--				- Juan Carlos Valderrama Camargo
+
+------------------------------------------------------------------
+--Borrado de datos de la BD
+------------------------------------------------------------------
