8066f131b7e6d8b51eeb7d781eebc30c3d80a7de
juancarlosvalderramacamargo <juancarlosvalderramacamargo@gmail.com>
2019-04-15 18:38:42 -0500

    cambio UI

--- Proyectotransmilenio/App_Start/BundleConfig.cs
+++ Proyectotransmilenio/App_Start/BundleConfig.cs
@@ -26,6 +26,30 @@ namespace Proyectotransmilenio
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/template/css").Include(
+                      "~/template/vendor/bootstrap/css/bootstrap.min.css",
+                      "~/template/vendor/mentisMenu/metisMenu.min.css",
+                      "~/template/vendor/datatables-plugins/dataTables.bootstrap.css",
+                      "~/template/vendor/datatables-responsive/dataTables.responsive.css",
+                      "~/template/dist/css/sb-admin-2.css",
+                      "~/template/vendor/morrisjs/morris.css",
+                      "~/template/vendor/font-awesome/css/font-awesome.min.css",
+                       "~//Content/uploadify.css"));
+
+            bundles.Add(new ScriptBundle("~/template/bootstrap").Include(
+                      "~/template/vendor/jquery/jquery.min.js",
+                      "~/template/vendor/bootstrap/js/bootstrap.min.js",
+                      "~/template/vendor/mentisMenu/metisMenu.min.js",
+                      "~/template/vendor/raphael/raphael.min.js",
+                      "~/template/vendor/morrisjs/morris.min.js",
+                      "~/template/data/morris-data.js",
+                      "~/template/vendor/datatables/js/jquery.dataTables.min.js",
+                      "~/template/vendor/datatables-plugins/dataTables.bootstrap.min.js",
+                      "~/template/vendor/datatables-responsive/dataTables.responsive.js",
+                      "~/template/dist/js/sb-admin-2.js",
+                      "~/Scripts/jquery.uploadify.v2.1.4.min.js",
+                      "~/Scripts/swfobject.js"));
         }
     }
 }
